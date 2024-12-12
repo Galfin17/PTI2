6c63df07c9a243e9d214c474b59dc5eed6ccc292
Galfin17 <123260652+Galfin17@users.noreply.github.com>
2024-12-12 08:38:27 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,63 +1,55 @@
 <?php
-include 'koneksi.php';
+$url = "https://tokobandung221.000webhostapp.com/admin/api/produk.php";
+
+// inisialisasi curl
+$ch = curl_init();
+
+// mengatur URL yang ingin di load
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// mengembalikan data pada URL dalam bentuk String
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// mengeksekusi curl
+$output = curl_exec($ch);
+
+// Karena data yang di keluarkan dalam bentuk JSON
+// Maka data output tersebut harus di Decode kedalam bentuk OBJECT
+$result = json_decode($output);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Fashion</title>
+  <title>Fashion</title>
 </head>
 <body>
-<?php
-if (isset($_GET["hapus"])) {
-// Query untuk menghapus data
-$sql = "DELETE FROM produk WHERE id_produk = " . $_GET["hapus"];
-if ($conn->query($sql) === TRUE) {
-// Mengalihkan halaman
-header("Location: http://localhost/tokoberkah/admin");
-}
-}
-?>
-
-<h1>Berkah Fashion - Admin</h1>
-<table >
-<?php
-$sql = "SELECT * FROM produk";
-$result = $conn->query($sql);
-$i = 0;
-if ($result->num_rows > 0) {
-while($row = $result->fetch_assoc()) {
-$i++;
-echo ' <tr>
-<td width="30px"><center>'.$i.'</center></td>
-<td><img width="100px" src="image/'.$row["foto"].'"/></td>
-<td width="200px">'.$row["nama_produk"].'</td>
-<td width="100px">Rp'.number_format($row["harga"], 0, '.', '.').'</td>
-<td>
-<button><a href="ubah.php?id='.$row["id_produk"].'">Ubah</a></button>
-<button
-onclick="hapus('.$row["id_produk"].',`'.$row["nama_produk"].'`)">Hapus</button>
-</td>
-</tr>';
-}
-} else {
-echo "Belum ada produk";
-}
-?>
-</table>
-
-<br>
-
-<button><a href="tambah.php">Tambah Produk baru</a></button>
-
+  <h1>Toko Baju</h1>
+  <table>
+    <?php
+    // Mengecek apakah result memiliki response list produk atau tidak
+    if (count($result->data) > 0) {
+      // Melooping data result dan menampilkan data tersebut ke dalam view
+      foreach ($result->data as $row) {
+        echo ' <tr>
+          <td><img width="50px" src="' . $row->foto . '"/></td>
+          <td width="200px">' . $row->nama_produk . '</td>
+          <td width="100px">Rp' . number_format($row->harga, 0, '.', '.') . '</td>
+          <td>
+            <button onclick="hapus('.$row->id_produk.', `'.$row->nama_produk.'`)">Hapus</button>
+          </td>
+        </tr>';
+      }
+    }
+    ?>
+  </table>
 </body>
-
 <script>
-function hapus(id, nama_produk) {
-if (confirm('Apakah anda yakin ' + nama_produk + ' akan di hapus? ')) {
-window.location.replace(`<?= getBaseUrl("index.php?hapus=") ?>`+ id);
-
-}
-}
+  //Menghapus produk
+  function hapus(id, nama_produk) {
+    if (confirm('Apakah anda yakin ' + nama_produk + ' akan di hapus? ')) {
+      window.location.replace("https://tokobandung221.000webhostapp.com/admin/api/hapus.php?id=" + id);
+    }
+  }
 </script>
 </html>
