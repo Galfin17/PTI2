c460a61a89edab5c9e77ec5fe336f2ad05536488
Atila004 <123058894+Atila004@users.noreply.github.com>
2024-12-12 08:43:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,62 @@
-<?php
-$url = "https://tokobandung221.000webhostapp.com/admin/api/produk.php";
-
-// inisialisasi curl
-$ch = curl_init();
-
-// mengatur URL yang ingin di load
-curl_setopt($ch, CURLOPT_URL, $url);
-
-// mengembalikan data pada URL dalam bentuk String
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-// mengeksekusi curl
-$output = curl_exec($ch);
-
-// Karena data yang di keluarkan dalam bentuk JSON
-// Maka data output tersebut harus di Decode kedalam bentuk OBJECT
-$result = json_decode($output);
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
   <title>Fashion</title>
 </head>
 <body>
-  <h1>Toko Baju</h1>
+  <h1>Berkah Fashion - Cabang Dago</h1>
   <table>
+    <form action="#" method="post" enctype="multipart/form-data">
+      <table>
+        <tr>
+          <td width="100px">Url</td>
+          <td><input type="text" name="url" placeholder="Masukkan url"></td>
+        </tr>
+      </table>
+      <br>
+      <tr>
+        <input type="submit" value="Load" name="submit"/>
+      </tr>
+    </form>
+  </table>
+
+  <table>
+    <br>
+    <br>
     <?php
-    // Mengecek apakah result memiliki response list produk atau tidak
-    if (count($result->data) > 0) {
-      // Melooping data result dan menampilkan data tersebut ke dalam view
-      foreach ($result->data as $row) {
-        echo ' <tr>
-          <td><img width="50px" src="' . $row->foto . '"/></td>
-          <td width="200px">' . $row->nama_produk . '</td>
-          <td width="100px">Rp' . number_format($row->harga, 0, '.', '.') . '</td>
-          <td>
-            <button onclick="hapus('.$row->id_produk.', `'.$row->nama_produk.'`)">Hapus</button>
-          </td>
-        </tr>';
+    if (isset($_POST["submit"])) {
+      $url = $_POST["url"];
+      // inisialisasi curl
+      $ch = curl_init();
+      // mengaatur URL yang ingin di load
+      curl_setopt($ch, CURLOPT_URL, $url);
+      // mengembalikan data pada URL dalam bentuk String
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      // mengeksekusi curl
+      $output = curl_exec($ch);
+
+      // Karena data yang di keluarkan dalam bentuk JSON
+      // Maka data output tersebut harus di Decode kedalam bentuk OBJECT
+      $result = json_decode($output);
+
+      // Inisialisasi nomor urutan
+      $i = 0;
+
+      // Mengecek apakah result memiliki data atau tidak
+      if (count($result->data) > 0) {
+        // Melooping data result dan menampilkan data tersebut
+        foreach ($result->data as $row) {
+          $i++;
+          echo ' <tr>
+            <td width="30px"><center>' . $i . '</center></td>
+            <td><img width="50px" src="' . $row->foto . '"/></td>
+            <td>' . $row->nama_produk . '</td>
+            <td width="100px">Rp' . number_format($row->harga, 0, '.', '.') . '</td>
+          </tr>';
+        }
       }
     }
     ?>
   </table>
 </body>
-<script>
-  //Menghapus produk
-  function hapus(id, nama_produk) {
-    if (confirm('Apakah anda yakin ' + nama_produk + ' akan di hapus? ')) {
-      window.location.replace("https://tokobandung221.000webhostapp.com/admin/api/hapus.php?id=" + id);
-    }
-  }
-</script>
 </html>
